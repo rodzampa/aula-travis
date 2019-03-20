cd2f7dafa9a613d195241abbf632277300066c0f
rzampa <rzampa1979@gmail.com>
2019-03-20 10:36:03 -0300

    arquivo 1

---
+++ principal.py
@@ -0,0 +1,9 @@
+def somar(x,y):
+    return x+y
+
+def subtrair(x,y):
+    return x-y
+
+def mult (x,y):
+    pass
+
---
+++ teste.py
@@ -0,0 +1,5 @@
+import pytest
+from principal import somar 
+
+def test_soma():
+    assert somar (2,4)==6
